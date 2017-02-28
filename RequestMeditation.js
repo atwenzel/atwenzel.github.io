c6053b6c8f35edb7096a561317be580e2715ecd5
xkc1100 <xkc1100@gmail.com>
2017-02-27 19:49:19 -0600

    Add files via upload
    Add two pages, request meditation and timer

---
+++ RequestMeditation.js
@@ -0,0 +1,6 @@
+var submit = document.getElementById("submit");
+var MinutesAvailable = document.getElementById("MinutesAvailable")
+
+submit.onclick = function(e) {
+    var InitialTimerValue = {MinutesAvailable: MinutesAvailable.value};
+};
