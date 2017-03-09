c163c12722dff4d5bed5ddc56cacc776d7d86133
Alex <alexanderwenzel2017@u.northwestern.edu>
2017-03-08 22:32:46 -0600

    fixed stress cause in table

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var drink = document.getElementById("drink")
 
 //Post code inspired by http://stackoverflow.com/questions/24468459/sending-a-json-to-server-and-retrieving-a-json-in-return-without-jquery
 submit_b.onclick = function(e) {
-    var obj = {mood: mood_s.value, sleep: sleep.value, cause: checkbox.value, drinks: drink.value};
+    var obj = {mood: mood_s.value, sleep: sleep.value, cause: checkbox.checked, drinks: drink.value};
     xhr = new XMLHttpRequest();
     var url = "http://houndstooth.cs.northwestern.edu:49494";
     xhr.open("POST", url, true)
